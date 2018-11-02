b3d6db794f6b1e103ce2241d32bf5119f6dbaf0b
hbachchas <himanshu7buckchash@gmail.com>
2018-11-02 13:58:21 +0530

    n

--- crawler/google-images-download/download.sh
+++ crawler/google-images-download/download.sh
@@ -3,7 +3,10 @@
 # Project: https://github.com/hardikvasa/google-images-download
 
 # to download over 100 images; chromedriver has to be used
-googleimagesdownload --keywords "tomato" --limit 200 --chromedriver "./chromedriver"
+
+source activate e1_dl_py3.5     # switch to virtual environment
+googleimagesdownload --keywords "tomato" --limit 10 --chromedriver "./chromedriver"
 # googleimagesdownload --keywords "tomato" --offset 210 --limit 310 --chromedriver "./chromedriver"
     # above command will download 100 images
     # the size argument can be used to specify size of the images to be downloaded
+source deactivate
