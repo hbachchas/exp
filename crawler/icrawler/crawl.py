956a00877d291f83cd139dab6ff9c1ab842587f0
hbachchas <himanshu7buckchash@gmail.com>
2018-11-02 12:48:17 +0530

    stable

--- crawler/icrawler/crawl.py
+++ crawler/icrawler/crawl.py
@@ -5,17 +5,12 @@ google_crawler = GoogleImageCrawler(
     parser_threads=2,
     downloader_threads=4,
     storage={'root_dir': '/home/himanshu/Documents/Projects/GIT/myexp/crawler/icrawler/download'})
+    # FOR MORE ON FILTERS : icrawler/builtin/google.py
 filters = dict(
-    size='large',
-    color='orange',
+    size='=200x200',        # large, medium, icon, >[]x[], =[]x[] ([] is an integer)          e.g. '=100x100'
+    type='photo',           # photo, face, clipart, linedrawing, animated
+    # color='orange',
     # license='commercial,modify',
     # date=((2017, 1, 1), (2017, 11, 30))
     )
-google_crawler.crawl(keyword='cat', filters=filters, max_num=10, file_idx_offset=0)
-
-
-
-
-# google_crawler = GoogleImageCrawler(storage={'root_dir': ''})
-# google_crawler.crawl(keyword='cat', max_num=10)
-# google_crawler.crawl()
+google_crawler.crawl(keyword='natural pumpkin', filters=filters, max_num=20, file_idx_offset=0)
